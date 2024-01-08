71816801a79a333a351b5d757135d05002ef4283
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2024-01-08 21:56:15 +0530

    solved

---
+++ binarySearch.cpp
@@ -0,0 +1,38 @@
+/*Problem statement
+You are given an integer array 'A' of size 'N', sorted in non-decreasing order. You are also given an integer 'target'.
+Your task is to write a function to search for 'target' in the array 'A'. If it exists, return its index in 0-based indexing. If 'target' is not present in the array 'A', return -1.
+Note:
+You must write an algorithm whose time complexity is O(LogN)*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int binarySearch(vector<int> &nums, int target)
+{
+    int n = nums.size();
+    int low = 0, high = n - 1;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+        if (nums[mid] == target)
+            return mid;
+        else if (target > nums[mid])
+            low = mid + 1;
+        else
+            high = mid - 1;
+    }
+    return -1;
+}
+
+int main()
+{
+    vector<int> a = {3, 4, 6, 7, 9, 12, 16, 17};
+    int target = 6;
+    int ind = binarySearch(a, target);
+    if (ind == -1)
+        cout << "The target is not present." << endl;
+    else
+        cout << "The target is at index: "
+             << ind << endl;
+    return 0;
+}
