31c9cc413cce49f3fe94f64ad482abf4e4be35fa
kamrul17 <72572933+kamrul17@users.noreply.github.com>
2024-01-09 22:26:30 +0530

    solved

---
+++ SearchInsertPosition.cpp
@@ -0,0 +1,39 @@
+/*Example 1:
+Input Format: arr[] = {1,2,4,7}, x = 6
+Result: 3
+Explanation: 6 is not present in the array. So, if we will insert 6 in the 3rd index(0-based indexing), the array will still be sorted. {1,2,4,6,7}.*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int searchInsert(vector<int> &arr, int x)
+{
+    int n = arr.size();
+    int low = 0, high = n - 1;
+    int ans = n;
+
+    while (low <= high)
+    {
+        int mid = (low + high) / 2;
+
+        if (arr[mid] >= x)
+        {
+            ans = mid;
+
+            high = mid - 1;
+        }
+        else
+        {
+            low = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main()
+{
+    vector<int> arr = {1, 2, 4, 7};
+    int x = 6;
+    int ind = searchInsert(arr, x);
+    cout << "The index is: " << ind << "\n";
+    return 0;
+}
